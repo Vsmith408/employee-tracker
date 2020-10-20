ab807ded8fa78b75ccefac671d4d6c5c6d9d850e
Victoria Smith <victoriasmithh428@gmail.com>
2020-10-20 13:04:36 -0400

    finished creating app

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-var mysql = require('mysql')
-var inquirer = require('inquirer')
+const mysql = require('mysql')
+const inquirer = require('inquirer')
+const cTable = require('console.table')
+const { throwError } = require('rxjs')
 
-var connection = mysql.createConnection({
+console.clear()
+
+const connection = mysql.createConnection({
   host: 'localhost',
 
   // Your port; if not 3306
-  port: 3000,
+  port: 3306,
 
   // Your username
   user: 'root',
@@ -19,3 +23,282 @@ connection.connect(function (err) {
   if (err) throw err
   runTracker()
 })
+
+function runTracker() {
+  inquirer
+    .prompt({
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'Create department',
+        'View departments',
+        'Create role',
+        'View roles',
+        'Create employee',
+        'View employees',
+        'Update employee role',
+      ],
+      name: 'action',
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case 'Create department':
+          newDepartment()
+          break
+
+        case 'View departments':
+          viewDepartments()
+          break
+
+        case 'Create role':
+          newRole()
+          break
+
+        case 'View roles':
+          viewRoles()
+          break
+
+        case 'Create employee':
+          newEmployee()
+          break
+
+        case 'View employees':
+          viewEmployees()
+          break
+
+        case 'Update employee role':
+          updateEmployee()
+          break
+      }
+    })
+}
+
+// Department handlers
+function viewDepartments() {
+  let query = 'SELECT * FROM department'
+  connection.query(query, function (err, res) {
+    if (err) {
+      throw Error(err.message)
+    }
+    console.clear()
+    console.table(res)
+    runTracker()
+  })
+}
+
+function newDepartment() {
+  inquirer
+    .prompt({
+      type: 'input',
+      message: 'What is the departments name?',
+      name: 'name',
+    })
+    .then(function (answer) {
+      let query = 'INSERT INTO department (name) VALUES (?)'
+      connection.query(query, [answer.name], function (err, res) {
+        if (err) {
+          throw Error(err.message)
+        }
+        console.clear()
+        console.log('Department Created!')
+        runTracker()
+      })
+    })
+}
+
+//============================================================
+// Role handlers
+
+function newRole() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'What is the title of the role you want to add?',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'What is the salary for this role?',
+        name: 'salary',
+      },
+      {
+        type: 'input',
+        message: 'What is the department ID for this role?',
+        name: 'departmentId',
+      },
+    ])
+    .then((answer) => {
+      let query = 'SELECT id FROM department WHERE department.id = ?'
+      connection.query(query, [answer.departmentId], (err, res) => {
+        if (err) {
+          throw Error(err.message)
+        }
+        if (!res.length) {
+          throw Error('Invalid department ID')
+        }
+        let newQuery =
+          'INSERT INTO roles (title, salary, department_id) VALUES (?,?,?)'
+        connection.query(
+          newQuery,
+          [answer.title, answer.salary, answer.departmentId],
+          (err, res) => {
+            if (err) {
+              throw Error(err.message)
+            }
+            console.clear()
+            console.log('Role created!')
+            runTracker()
+          }
+        )
+      })
+    })
+}
+
+function viewRoles() {
+  let query = 'SELECT * FROM roles'
+  connection.query(query, function (err, res) {
+    if (err) {
+      throw Error(err.message)
+    }
+    console.clear()
+    console.table(res)
+    runTracker()
+  })
+}
+
+//============================================================
+// Employee handlers
+
+function newEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: "What is the employee's first name?",
+        name: 'firstName',
+      },
+      {
+        type: 'input',
+        message: "What is the employee's last name?",
+        name: 'lastName',
+      },
+      {
+        type: 'input',
+        message: 'What is the role id?',
+        name: 'roleId',
+      },
+      {
+        type: 'input',
+        message: "What is their manager's ID?",
+        name: 'managerId',
+      },
+    ])
+    .then((answer) => {
+      let query = 'SELECT id FROM roles WHERE roles.id = ?'
+      connection.query(query, [answer.roleId], (err, res) => {
+        if (err) {
+          throw Error(err.message)
+        }
+        if (!res.length) {
+          throw Error('Invalid role ID')
+        }
+        let query = 'SELECT id FROM employee WHERE employee.id = ?'
+        connection.query(query, [answer.managerId], (err, res) => {
+          if (answer.managerId) {
+            if (err) {
+              throw Error(err.message)
+            }
+            if (!res.length) {
+              throw Error('Invalid manager ID')
+            }
+          }
+          let newQuery =
+            'INSERT INTO employee (first_name, last_name, roles_id, manager_id) VALUES (?,?,?,?);'
+          connection.query(
+            newQuery,
+            [
+              answer.firstName,
+              answer.lastName,
+              answer.roleId,
+              answer.managerId || null,
+            ],
+            (err, res) => {
+              if (err) {
+                throw Error(err.message)
+              }
+              console.clear()
+              console.log('Employee created!')
+              runTracker()
+            }
+          )
+        })
+      })
+    })
+}
+
+function viewEmployees() {
+  let query = 'SELECT * FROM employee'
+  connection.query(query, function (err, res) {
+    if (err) {
+      throw Error(err.message)
+    }
+    console.clear()
+    console.table(res)
+    runTracker()
+  })
+}
+
+function updateEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'What is the employee ID?',
+        name: 'id',
+      },
+      {
+        type: 'input',
+        message: 'What is the updated role ID?',
+        name: 'roleId',
+      },
+    ])
+    .then((answers) => {
+      connection.query(
+        'SELECT id FROM employee WHERE employee.id = ?',
+        [answers.id],
+        (err, res) => {
+          if (err) {
+            throw Error(err.message)
+          }
+          if (!res.length) {
+            throw Error('Invalid employee ID')
+          }
+          connection.query(
+            'SELECT id FROM roles WHERE roles.id = ?',
+            [answers.roleId],
+            (err, res) => {
+              if (err) {
+                throw Error(err.message)
+              }
+              if (!res.length) {
+                throw Error('Invalid role ID')
+              }
+              connection.query(
+                'UPDATE employee SET roles_id = ? WHERE employee.id = ?',
+                [answers.roleId, answers.id],
+                (err, res) => {
+                  if (err) {
+                    throw Error(err.message)
+                  }
+                  console.clear()
+                  console.log('Employee updated!')
+                  runTracker()
+                }
+              )
+            }
+          )
+        }
+      )
+    })
+}
--- tracker_db.sql
+++ tracker_db.sql
@@ -6,14 +6,14 @@ USE tracker_db;
 
 CREATE TABLE department
 (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE role
+CREATE TABLE roles
 (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR (30),
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
@@ -22,10 +22,21 @@ CREATE TABLE role
 
 CREATE TABLE employee
 (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT NOT NULL,
+    roles_id INT NOT NULL,
     manager_id INT NULL,
     PRIMARY KEY (id)
 );
+
+INSERT INTO department (id, name) VALUES (1, "Product");
+INSERT INTO department (id, name) VALUES (2, "Engineering");
+INSERT INTO department (id, name) VALUES (3, "Design");
+
+INSERT INTO roles (id, title, salary, department_id) VALUES (1, "SWE", 100000.00, 2);
+INSERT INTO roles (id, title, salary, department_id) VALUES (2, "PO", 50000.00, 1);
+INSERT INTO roles (id, title, salary, department_id) VALUES (3, "UX Designer", 75000.00, 3);
+
+INSERT INTO employee (id, first_name, last_name, roles_id, manager_id) VALUES (1, "Jane", "Doe", 1, null);
+INSERT INTO employee (id, first_name, last_name, roles_id, manager_id) VALUES (2, "John", "Doe", 1, 1);
